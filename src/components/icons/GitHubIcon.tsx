e71512af5f5f0b3d847ce20f2b8096e0f1a8b964
Ahnaf Tahmid Chowdhury <tahmid@nukehub.org>
2026-08-27 14:13:20 +0600

    Add size prop to GitHubIcon

--- src/components/icons/GitHubIcon.tsx
+++ src/components/icons/GitHubIcon.tsx
@@ -1,8 +1,10 @@
-export function GitHubIcon({ className }: { className?: string }) {
+export function GitHubIcon({ className, size = 24 }: { className?: string; size?: number }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
+      width={size}
+      height={size}
       fill="none"
       stroke="currentColor"
       strokeWidth={2}
